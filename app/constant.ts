27468f65aa2f831da83cddffb11ffe9e64fe4b27
ayman-makki <manaykimak@gmail.com>
2024-07-02 17:56:50 +0100

    updated models

--- app/constant.ts
+++ app/constant.ts
@@ -147,13 +147,13 @@ const openaiModels = [
   "gpt-4-turbo",
   // "gpt-4-turbo-preview",
   "gpt-4o",
-  "gpt-4o-2024-05-13",
+  // "gpt-4o-2024-05-13",
   "gpt-4-vision-preview",
-  "gpt-4-turbo-2024-04-09",
+  // "gpt-4-turbo-2024-04-09",
 ];
 
 const googleModels = [
-  "gemini-1.0-pro",
+  // "gemini-1.0-pro",
   "gemini-1.5-pro-latest",
   "gemini-1.5-flash-latest",
   // "gemini-pro-vision",
@@ -161,15 +161,24 @@ const googleModels = [
 
 const anthropicModels = [
   // "claude-instant-1.2",
-  "claude-2.0",
-  "claude-2.1",
+  // "claude-2.0",
+  // "claude-2.1",
+  "claude-3-5-sonnet-20240620",
   "claude-3-sonnet-20240229",
   "claude-3-opus-20240229",
   "claude-3-haiku-20240307",
-  "claude-3-5-sonnet-20240620",
 ];
 
 export const DEFAULT_MODELS = [
+  ...anthropicModels.map((name) => ({
+    name,
+    available: true,
+    provider: {
+      id: "anthropic",
+      providerName: "Anthropic",
+      providerType: "anthropic",
+    },
+  })),
   ...openaiModels.map((name) => ({
     name,
     available: true,
@@ -186,15 +195,6 @@ export const DEFAULT_MODELS = [
       id: "google",
       providerName: "Google",
       providerType: "google",
-    },
-  })),
-  ...anthropicModels.map((name) => ({
-    name,
-    available: true,
-    provider: {
-      id: "anthropic",
-      providerName: "Anthropic",
-      providerType: "anthropic",
     },
   })),
 ] as const;
