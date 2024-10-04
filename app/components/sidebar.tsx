3b48303df4d9058fed2ae515cb848919c3100a74
ayman-makki <manaykimak@gmail.com>
2024-10-04 14:00:01 +0100

    update display

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -231,8 +231,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="Institut Malik Ibn Anas"
+        subTitle="S'entraîner avec l'IA"
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
