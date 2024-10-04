c5b32bbc26dd1385686122501f2404fec9febaec
ayman-makki <manaykimak@gmail.com>
2024-10-04 14:49:59 +0100

    new order of models

--- app/constant.ts
+++ app/constant.ts
@@ -307,9 +307,10 @@ const anthropicModels = [
   // "claude-2.0",
   // "claude-2.1",
   // "claude-3-sonnet-20240229",
+  "claude-3-5-sonnet-20240620",
+
   "claude-3-opus-20240229",
   "claude-3-haiku-20240307",
-  "claude-3-5-sonnet-20240620",
 ];
 
 const baiduModels = [
